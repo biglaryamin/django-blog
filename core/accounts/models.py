1cf2b0e09ce38a7cb5d3e774743153e12e41705c
amin <biglarymohamadamin1997.gmail.com>
2023-11-05 13:09:18 +0330

    add create_user function for UserManager

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,6 +1,26 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_field):
+        '''
+        create and save a user with the given email and password and extra data
+        '''
+        if not email:
+            raise ValueError(_("The email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_field):
+        '''
+        create and save a superuser with the given email and password and extra data
+        '''
+        pass
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255)
@@ -12,5 +32,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_Date = models.DateTimeField(auto_now=True)
 
+    objects = UserManager()
     def __str__(self) -> str:
         return self.email
