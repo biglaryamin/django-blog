c692f03a8e7d0ccb2daa7a7c50c8ad04d8c31efd
amin <biglarymohamadamin1997.gmail.com>
2023-11-05 13:10:14 +0330

    add is_superuser field in User model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=20)
     REQUIRED_FIELDS = []
